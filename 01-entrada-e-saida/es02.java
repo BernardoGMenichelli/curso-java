6d2a11294c4c92e222c86ee5a632a08020642bed
BernardoGMenichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-06-10 12:54:57 -0300

    Update es02.java

--- 01-entrada-e-saida/es02.java
+++ 01-entrada-e-saida/es02.java
@@ -9,13 +9,13 @@ import java.util.Scanner;
 public class Entrada_de_dados_2 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         //Ler texto até a quebra de linha
         System.out.println("Digite três linhas de texto:");
-        String x1 = scanner.nextLine();
-        String x2 = scanner.nextLine();
-        String x3 = scanner.nextLine();
+        String x1 = sc.nextLine();
+        String x2 = sc.nextLine();
+        String x3 = sc.nextLine();
 
         System.out.println("\nDados digitados:");
         System.out.println(x1);
@@ -24,14 +24,14 @@ public class Entrada_de_dados_2 {
 
         // Ler um número inteiro
         System.out.println("\nDigite um número inteiro:");
-        int y = scanner.nextInt();
-        scanner.nextLine(); //Limpa o buffer de leitura
+        int y = sc.nextInt();
+        sc.nextLine(); //Limpa o buffer de leitura
 
         // Ler mais três linhas de texto
         System.out.println("Digite mais três linhas de texto:");
-        String z1 = scanner.nextLine();
-        String z2 = scanner.nextLine();
-        String z3 = scanner.nextLine();
+        String z1 = sc.nextLine();
+        String z2 = sc.nextLine();
+        String z3 = sc.nextLine();
 
         System.out.println("\nDados digitados:");
         System.out.println(y);
@@ -39,6 +39,6 @@ public class Entrada_de_dados_2 {
         System.out.println(z2);
         System.out.println(z3);
 
-        scanner.close();
+        sc.close();
     }
 }
