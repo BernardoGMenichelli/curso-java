9aa395b84ffdfa358c3ca21d534e00288bc2ef8e
BernardoGMenichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-06-23 11:30:23 -0300

    Create 01-while.java

---
+++ 03-estrutura-repetitiva/01-while.java
@@ -0,0 +1,29 @@
+package com.mycompany.estrutura_repetitiva;
+/**
+ * Estrutura repetitiva while em Java.
+ * Autor: Bernardo
+ * Data: Junho de 2025
+ */
+import java.util.Scanner;
+
+public class Estrutura_repetitiva {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite um número inteiro: ");
+        int x = sc.nextInt();
+        
+        int soma = 0;
+        //V: executa e vola
+        //F: pula fora
+        while(x != 0){ //quando digitar 0 o while se torna false e para de repetir
+            soma += x;
+            x = sc.nextInt();
+        }
+        
+        System.out.println("Soma = " + soma);
+        
+        sc.close();
+    }
+}
