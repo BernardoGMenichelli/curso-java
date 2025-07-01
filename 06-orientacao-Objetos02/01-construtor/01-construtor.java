3daf9d7820a50733ec58a47eec33a9d50e8c9a50
BernardoGMenichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-07-01 09:18:17 -0300

    Create 01-construtor.java

---
+++ 06-orientacao-Objetos02/01-construtor/01-construtor.java
@@ -0,0 +1,48 @@
+package com.mycompany.construtores;
+/**
+ * Método construtor em Java.
+ * Autor: Bernardo
+ * Data: Junho de 2025
+ */
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Product;
+
+public class Construtores {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+
+        System.out.println("Insira os dados do produto:");
+        System.out.print("Nome: ");
+        String name = sc.nextLine();
+        System.out.print("Preço: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantidade no estoque: ");
+        int quantity = sc.nextInt();
+         
+        Product produto = new Product(name, price, quantity);
+        
+        System.out.println();
+        System.out.println("Dados do produto: " + produto); 
+        
+        System.out.println();
+        System.out.print("Insira o número de produtos a serem adicionados ao estoque: ");
+        quantity = sc.nextInt();
+        produto.addProducts(quantity);
+        
+        System.out.println();
+        System.out.println("Dados atualizados: " + produto);
+        
+        System.out.println();
+        System.out.print("Insira o número de produtos a serem removidos do estoque: ");
+        quantity = sc.nextInt();
+        produto.removeProducts(quantity);
+        
+        System.out.println();
+        System.out.println("Dados atualizados: " + produto);
+        
+        sc.close();
+    }
+}
