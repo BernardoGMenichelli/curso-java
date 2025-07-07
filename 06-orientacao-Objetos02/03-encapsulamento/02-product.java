4aceef45eeae5a9ed332d0c90f0faed43ce42b5f
BernardoGMenichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-07-07 11:26:20 -0300

    Update 02-product.java

--- 06-orientacao-Objetos02/03-encapsulamento/02-product.java
+++ 06-orientacao-Objetos02/03-encapsulamento/02-product.java
@@ -1,1 +1,65 @@
+package entities;
 
+public class Product {
+    //atributos
+    private String name;
+    private double price;
+    private int quantity;
+    
+    //construtores
+
+    public Product() {}
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+    
+    //encapsulamento
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    //metodos
+    public double totalValueInStock(){
+        return price*quantity;
+    }
+    
+    public void addProducts(int quantity){
+        //O this acessa o atributo da classe e não o parametro da função 
+        this.quantity += quantity;
+    }
+    
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+    
+    //converte o objeto para String
+    public String toString(){
+        return name + ", R$ " + String.format("%.2f", price) + ", " + quantity + " unidades, Total: R$" + String.format("%.2f",totalValueInStock());
+    }
+}
