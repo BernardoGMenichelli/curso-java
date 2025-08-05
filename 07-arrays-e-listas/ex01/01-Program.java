e67159ab39e678da7a8df95ac3c45b3273fb68e6
BernardoGMenichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-08-04 21:21:45 -0300

    Create 01-Program.java

---
+++ 07-arrays-e-listas/ex01/01-Program.java
@@ -0,0 +1,38 @@
+package com.mycompany.exerc1;
+
+/**
+ * Exercício de array em Java. 
+ * Autor: Bernardo 
+ * Data: Junho de 2025
+ */
+import java.util.Scanner;
+
+public class Exerc1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quantos numeros voce vai digitar(limite de 10)? ");
+        int n = sc.nextInt();
+
+        if (n <= 10) {
+            int[] vect = new int[n];
+
+            for (int i = 0; i < vect.length; i++) {
+                System.out.print("Digite um numero: ");
+                vect[i] = sc.nextInt();
+            }
+
+            System.out.println("Numeros negativos: ");
+            for (int i = 0; i < vect.length; i++) {
+                if (vect[i] < 0) {
+                    System.out.println(vect[i]);
+                }
+            }
+
+        } else {
+            System.out.println("O valor excede o limite!");
+
+        }
+        sc.close();
+    }
+}
