1611624c88a70a72d0f4f9983c1665ea05e603db
BernardoGMenichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-08-04 21:44:14 -0300

    Create 01-Program.java

---
+++ 07-arrays-e-listas/ex07/01-Program.java
@@ -0,0 +1,40 @@
+package com.mycompany.exerc7;
+
+/**
+ * Exercício de array em Java. 
+ * Autor: Bernardo 
+ * Data: Junho de 2025
+ */
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exerc7 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        int n = sc.nextInt();
+
+        double[] vect = new double[n];
+        double soma = 0;
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Digite um numero: ");
+            vect[i] = sc.nextDouble();
+
+            soma += vect[i];
+        }
+
+        System.out.println();
+        System.out.printf("Media do vetor = %.3f%n", soma / n);
+
+        System.out.println("Elementos abaixo da media: ");
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] < soma / n) {
+                System.out.println(vect[i]);
+            }
+        }
+        sc.close();
+    }
+}
