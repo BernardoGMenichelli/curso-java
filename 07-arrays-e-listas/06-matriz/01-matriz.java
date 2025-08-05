9a0d8043db93db2fbd7a96aeea0c4068d2f9ab31
BernardoGMenichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-08-04 21:06:38 -0300

    Create 01-matriz.java

---
+++ 07-arrays-e-listas/06-matriz/01-matriz.java
@@ -0,0 +1,43 @@
+package com.mycompany.matriz;
+
+/**
+ * Matriz em Java.
+ * Autor: Bernardo
+ * Data: Junho de 2025
+ */
+import java.util.Scanner;
+
+public class Matriz {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] mat = new int[n][n];
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Diagonal principal:");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+
+        int count = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+        
+        System.out.println();
+        System.out.println("Numeros negativos = " + count);
+        
+        sc.close();
+    }
+}
