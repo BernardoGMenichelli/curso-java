f11fd99062cdf1fca8ebfb95907262572d20c7c7
BernardoGMenichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-08-04 21:39:15 -0300

    Create 01-Program.java

---
+++ 07-arrays-e-listas/ex06/01-Program.java
@@ -0,0 +1,39 @@
+package com.mycompany.exerc6;
+
+/**
+ * Exercício de array em Java. 
+ * Autor: Bernardo 
+ * Data: Junho de 2025
+ */
+import java.util.Scanner;
+
+public class Exerc6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos valores vai ter cada vetor? ");
+        int n = sc.nextInt();
+
+        int[] vectA = new int[n];
+        int[] vectB = new int[n];
+        int[] vectC = new int[n];
+
+        System.out.println("Digite os valores do vetor A: ");
+        for (int i = 0; i < vectA.length; i++) {
+            vectA[i] = sc.nextInt();
+        }
+
+        System.out.println("Digite os valores do vetor B: ");
+        for (int i = 0; i < vectB.length; i++) {
+            vectB[i] = sc.nextInt();
+        }
+
+        System.out.println("Vetor resultante:");
+        for (int i = 0; i < vectC.length; i++) {
+            vectC[i] = vectA[i] + vectB[i];
+            System.out.println(vectC[i]);
+        }
+        sc.close();
+    }
+}
