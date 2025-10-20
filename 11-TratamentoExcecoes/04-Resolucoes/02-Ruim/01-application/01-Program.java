dc98e7a50b33e3c7ab6b1486fb08edbf956b045d
Bernardo Galloni Menichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-10-20 08:40:40 -0300

    Create 01-Program.java

---
+++ 11-TratamentoExcecoes/04-Resolucoes/02-Ruim/01-application/01-Program.java
@@ -0,0 +1,50 @@
+package application;
+
+/**
+ * Tratamento de exceções em Java. 
+ * Autor: Bernardo 
+ * Data: outubro de 2025
+ */
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import model.entities.Reservation;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        LocalDate checkIn = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        LocalDate checkOut = LocalDate.parse(sc.next(), fmt);
+
+        if (!checkOut.isAfter(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date!");
+        } else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println(reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = LocalDate.parse(sc.next(), fmt);
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = LocalDate.parse(sc.next(), fmt);
+
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null) {
+                System.out.println("Error in reservation" + error);
+            } else {
+                System.out.println(reservation);
+            }
+
+        }
+        sc.close();
+    }
+}
