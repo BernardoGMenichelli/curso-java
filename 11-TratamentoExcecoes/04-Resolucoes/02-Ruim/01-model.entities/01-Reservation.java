aea3a0454825b5abbd23943299d7a84e8989a4f7
Bernardo Galloni Menichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-10-20 08:41:55 -0300

    Create 01-Reservation.java

---
+++ 11-TratamentoExcecoes/04-Resolucoes/02-Ruim/01-model.entities/01-Reservation.java
@@ -0,0 +1,68 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    private static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+        return ChronoUnit.DAYS.between(checkIn, checkOut);
+    }
+
+    public String updateDates(LocalDate checkIn, LocalDate checkOut) {
+
+        LocalDate now = LocalDate.now();
+        if (checkIn.isBefore(now) || checkOut.isBefore(now)) {
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if (!checkOut.isAfter(checkIn)) {
+            return "Error in reservation: Check-out date must be after check-in date!";
+        }
+
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation: Room "
+                + roomNumber
+                + ", check-in: "
+                + checkIn.format(fmt)
+                + ", check-out: "
+                + checkOut.format(fmt)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
