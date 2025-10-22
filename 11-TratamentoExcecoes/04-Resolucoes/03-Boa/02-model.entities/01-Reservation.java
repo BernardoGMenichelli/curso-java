733e8efaa5ed1db151569078bcbb349f73436f11
Bernardo Galloni Menichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2025-10-22 08:00:06 -0300

    Create 01-Reservation.java

---
+++ 11-TratamentoExcecoes/04-Resolucoes/03-Boa/02-model.entities/01-Reservation.java
@@ -0,0 +1,72 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import model.exceptions.DomainException;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    private static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) throws DomainException {
+        if (!checkOut.isAfter(checkIn)) {
+            throw new DomainException("Error in reservation: Check-out date must be after check-in date!");
+        }
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+        return ChronoUnit.DAYS.between(checkIn, checkOut);
+    }
+
+    //pode lançar uma exceção dependendo do que acontecer
+    public void updateDates(LocalDate checkIn, LocalDate checkOut) throws DomainException {
+
+        LocalDate now = LocalDate.now();
+        if (checkIn.isBefore(now) || checkOut.isBefore(now)) {
+            throw new DomainException("Error in reservation: Reservation dates for update must be future dates");
+        }
+        if (!checkOut.isAfter(checkIn)) {
+            throw new DomainException("Error in reservation: Check-out date must be after check-in date!");
+        }
+
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation: Room "
+                + roomNumber
+                + ", check-in: "
+                + checkIn.format(fmt)
+                + ", check-out: "
+                + checkOut.format(fmt)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
