6668ffc996b50d76f6b044886ad8b99d2aed73b0
Bernardo Galloni Menichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2026-01-08 11:24:51 -0300

    Create 01-Program.java

---
+++ 12-TrabalhandoComArquivos/02-ClassesReader/01-Program.java
@@ -0,0 +1,48 @@
+package com.mycompany.classesreader;
+
+/**
+ * Lendo arquivos de texto em Java.
+ * Autor: Bernardo
+ * Data: Novembro de 2025
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ClassesReader {
+
+    public static void main(String[] args) {
+
+        String path = "c:\\temp\\out.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            //BufferedReader é instanciado a partir do FileReader(abstração maior)
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
