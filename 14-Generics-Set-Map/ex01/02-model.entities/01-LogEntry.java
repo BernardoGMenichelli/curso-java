def972c992d2e1b2a9ad6eab0008c369a021c997
Bernardo Galloni Menichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2026-03-25 17:46:21 -0300

    Create 01-LogEntry.java

---
+++ 14-Generics-Set-Map/ex01/02-model.entities/01-LogEntry.java
@@ -0,0 +1,54 @@
+package model.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class LogEntry {
+
+    private String userName;
+    private Instant moment;
+
+    public LogEntry(String userName, Instant moment) {
+        this.userName = userName;
+        this.moment = moment;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.userName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LogEntry other = (LogEntry) obj;
+        return Objects.equals(this.userName, other.userName);
+    }
+
+}
