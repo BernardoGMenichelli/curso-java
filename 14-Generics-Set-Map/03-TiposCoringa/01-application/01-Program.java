72033bd193c1b014d30bf9dc3b339263cd858c8d
Bernardo Galloni Menichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2026-03-25 17:05:16 -0300

    Create 01-Program.java

---
+++ 14-Generics-Set-Map/03-TiposCoringa/01-application/01-Program.java
@@ -0,0 +1,29 @@
+package application;
+
+/**
+ * Tipos coringa em Java.
+ * Autor: Bernardo
+ * Data: Fevereiro de 2026
+ */
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(5, 2, 10);
+        printList(myInts);
+        
+        List<String> myStrs = Arrays.asList("Maria","Alex","Bob");//pesquisar oque é esse metodo
+        printList(myStrs);
+    }
+    
+       
+
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
+    }
+}
