5239cae3f7f1a8fe98e0f9b0e91d73b7f1819650
Bernardo Galloni Menichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2026-03-25 16:55:14 -0300

    Create 01-Product.java

---
+++ 14-Generics-Set-Map/02-GenericsDelimitado/02-model.entities/01-Product.java
@@ -0,0 +1,42 @@
+package model.entities;
+
+public class Product implements Comparable<Product>{
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString(){
+        return name + ", " + String.format("%.2f", price);
+    }
+
+    @Override
+    public int compareTo(Product o) {
+        int result = price.compareTo(o.getPrice());
+        
+        if (result != 0){
+            return result;
+        }
+        return name.compareTo(o.getName());
+    }
+}
