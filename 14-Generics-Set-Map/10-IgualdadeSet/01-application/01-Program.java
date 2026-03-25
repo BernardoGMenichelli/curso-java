87f404244b8cb50aad3a088773f5a97337def60c
Bernardo Galloni Menichelli <167005212+BernardoGMenichelli@users.noreply.github.com>
2026-03-25 17:35:35 -0300

    Create 01-Program.java

---
+++ 14-Generics-Set-Map/10-IgualdadeSet/01-application/01-Program.java
@@ -0,0 +1,26 @@
+package application;
+
+/**
+ * Como Set testa igualdade em Java.
+ * Autor: Bernardo
+ * Data: Março de 2026
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+import model.entities.Product;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Set<Product> set = new HashSet<>();
+        
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+        
+        Product prod = new Product("Notebook", 1200.0);
+        
+        System.out.println(set.contains(prod));
+    }
+}
